e772163d660e06d6aee7cd3e8f29abb4db1fd1d1
JVictorV <joaovictorvisona@gmail.com>
2020-07-03 17:37:58 -0300

    Initial dev setup

---
+++ templates/darkmodern/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	theme: {
+		fontFamily: {
+			noto: ['Noto Sans JP', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				primary: '#4aff57',
+				secondary: '#23263a',
+				'secondary-light': '#2b2d41',
+			},
+		},
+	},
+};
